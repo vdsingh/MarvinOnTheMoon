afbd9ca5ff79208b20f3675e0445b6f2304c5530
rwbakerUMASS <rwbaker@umass.edu>
2022-12-14 15:32:54 -0500

    Carry objects and Add AI Prefab

--- Assets/Scripts/ChangableGravity.cs
+++ Assets/Scripts/ChangableGravity.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class ChangableGravity : MonoBehaviour
 {
     internal float gravity = -1.62f;
-    
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Physics.gravity = new Vector3(0, gravity, 0);
+        rb.AddForce(0, gravity, 0);
     }
+
+    public void ClearVelocity()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
 }
