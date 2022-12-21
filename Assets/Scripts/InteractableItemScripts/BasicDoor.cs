9b4a43edc77f4a2199dfce1811b45b854063e2a6
Vikram Singh <>
2022-12-20 21:10:39 -0500

    Add pressure plate to button prefab. Add conditions logic to door (for door open to require multiple conditions).

--- Assets/Scripts/InteractableItemScripts/BasicDoor.cs
+++ Assets/Scripts/InteractableItemScripts/BasicDoor.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BasicDoor : MonoBehaviour
 {
 
     public GameObject door;
     public bool isOpen = false;
+
+    // The number of conditions that must be met for the door to open
+    public bool useNumConditions = false;
+    public int numConditionsToMeet = 1;
+    private int numConditions = 0;
     // Start is called before the first frame update
+
+    [SerializeField] private UnityEvent numConditionsMet;
+    [SerializeField] private UnityEvent numConditionsNotMet;
     void Start()
     {
         if(isOpen)
@@ -16,7 +25,7 @@ public class BasicDoor : MonoBehaviour
         }
         else
         {
-             door.transform.localPosition = new Vector3(door.transform.localPosition.x, 0, door.transform.localPosition.z);
+            door.transform.localPosition = new Vector3(door.transform.localPosition.x, 0, door.transform.localPosition.z);
         }
         
     }
@@ -24,6 +33,8 @@ public class BasicDoor : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        Debug.Log("Num conditions met: " + numConditions);
         if(isOpen && door.transform.localPosition.y < 7.0f)
         {
             door.transform.localPosition += new Vector3(0, 0.01f * Time.time, 0);
@@ -45,5 +56,20 @@ public class BasicDoor : MonoBehaviour
     public void CloseDoor()
     {
         isOpen = false;
+    }
+
+    public void IncrementConditions()
+    {
+        numConditions++;
+        if(useNumConditions && numConditions >= numConditionsToMeet) {
+            numConditionsMet.Invoke();
+        }
+    }
+
+    public void DecrementConditions() {
+        numConditions--;
+        if(useNumConditions && numConditions < numConditionsToMeet) {
+            numConditionsNotMet.Invoke();
+        }
     }
 }
